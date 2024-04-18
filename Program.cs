9df5fe339f5378eba295a389e2ad05ff396376bc
Jorge Vilaça <jorge.vilaca@gmail.com>
2024-04-18 15:08:09 -0300

    solução projeto

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Smartphone iphone7 = new Iphone("1199999999", "7", "xpto", 8);
+iphone7.Ligar();
+iphone7.ReceberLigacao();
+iphone7.InstalarAplicativo("X");
+
+
+Smartphone nokia = new Nokia(numero: "1188888888", modelo: "NK015", iMEI: "wkrv", memoria: 8);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Facebook");
